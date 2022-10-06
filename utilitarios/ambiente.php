44435ca6fdf2a88076468569f2c439158c13e94c
Luconde Condé - Outlook <luconde@outlook.com>
2022-10-06 18:44:07 -0300

    Adicionando ambiente

---
+++ utilitarios/ambiente.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <p>
+            Banco de dados : <?php echo getenv("DB_DATABASE") ?>;
+        </p>
+        <p>
+            Host: <?php echo getenv("DB_HOST"); ?>
+        </p>
+        <p>
+            Usuario: <?php echo getenv("DB_USER"); ?>
+        </p>
+        <p>
+            Senha: <?php echo getenv("DB_PASSWORD"); ?>
+        </p>
+        <p>
+            Porta: <?php echo getenv("DB_PORT"); ?>
+        </p>
+    </body>
+</html>
